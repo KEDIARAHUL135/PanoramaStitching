2b3476bbc516b3ae3e50e86e9021d7cf0dbcfd87
KEDIARAHUL135 <kediarahul135@gmail.com>
2020-11-06 14:31:08 +0530

    Remove ReadImages Function
    This function is no longer needed for this part.

--- main.py
+++ main.py
@@ -3,36 +3,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-
-def ReadImage(InputImagePath):
-    Images = []                     # Input Images will be stored in this list.
-    
-	# If path is of a folder contaning images.
-    if os.path.isdir(InputImagePath):
-		# Getting the number of images present.
-        NumOfImgs = len(os.listdir(InputImagePath))
-
-        if NumOfImgs < 2:
-            print("\nSufficient number of images not found.\nProvide atleast 2 images.\n")
-            exit()
-
-        # Getting file extention (All image files must have same extention)
-        ext = os.path.splitext(os.listdir(InputImagePath)[0])[1]
-
-        for i in range(1, NumOfImgs+1):
-			# Reading images one by one.
-            InputImage = cv2.imread(InputImagePath + "/" + str(i) + ext)
-			
-            Images.append(InputImage)							# Storing images.
-        
-    # If it is not a folder.
-    else:
-        print("\nEnter valid Image Folder Path.\n")
-        exit()
-
-    return Images
-        
+      
 
 def FindMatches(BaseImage, SecImage):
     Sift = cv2.SIFT_create()
